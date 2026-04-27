d54e4df9413138fb7518af976dbe6879073352b1
lfelipeep <Luisfecardozoherrada@gmail.com>
2026-04-27 16:13:08 -0500

    Agregar script combinado gRPC

---
+++ finance_kids_api/grpc/start_grpc.py
@@ -0,0 +1,19 @@
+﻿import subprocess
+import threading
+import time
+
+def run_grpc():
+    subprocess.run(["python", "grpc_server.py"])
+
+def run_bridge():
+    time.sleep(3)
+    subprocess.run(["python", "grpc_bridge.py"])
+
+t1 = threading.Thread(target=run_grpc)
+t2 = threading.Thread(target=run_bridge)
+
+t1.start()
+t2.start()
+
+t1.join()
+t2.join()
